07d5b01f78d2e538d9fdb06493bfcb7c04c3e803
NehemiahBrown <nbrowndeveloper@gmail.com>
2026-01-15 18:19:44 -0600

    first commit

---
+++ popup.js
@@ -0,0 +1,11 @@
+const newPlayerBtn = document.getElementById("add-player");
+const playersField = document.getElementById("players-field");
+let player = document.getElementById("player");
+
+addPlayer.addEventListener("click", addPlayer);
+
+function addPlayer(){
+console.log("hello");
+}
+
+
